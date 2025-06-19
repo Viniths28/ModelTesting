84ac6cc28b5cac22beafd853ba00c8b814f3dee0
Vinith <vinithjoel28995@gmail.com>
2025-06-20 00:53:32 +1000

    setup file added to for making backend as import function to call flowengine

---
+++ flow_engine_project/backend/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="flow_engine",
+    version="0.1.0",
+    description="Graph-based Flow Builder Engine",
+    packages=find_packages(),
+    python_requires=">=3.8",
+) 
